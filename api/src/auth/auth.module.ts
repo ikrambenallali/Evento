32e8e8770a72c90f2fb020c8c57863cba7e2f85f
ikrambenallali <ikrambenallali2003@gmail.com>
2026-02-03 11:20:24 +0100

    EV-10 Créer l’entité User

--- api/src/auth/auth.module.ts
+++ api/src/auth/auth.module.ts
@@ -4,8 +4,17 @@ import { AuthService } from './auth.service';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { RolesGuard } from './guards/roles.guard';
 import { SelfOrAdminGuard } from './guards/self-or-admin.guard';
+import { UsersModule } from 'src/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+   imports: [
+    UsersModule,   
+    JwtModule.register({
+      secret: 'secret',
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
   controllers: [AuthController],
 
   providers: [
--- api/src/auth/guards/jwt-auth.guard.ts
+++ api/src/auth/guards/jwt-auth.guard.ts
@@ -2,11 +2,32 @@
 // =============================================================================================================================
 // Vérifie qu’un JWT valide est présent. Si non → 401 Unauthorized
 // =============================================================================================================================
-import { Injectable } from '@nestjs/common';
+import { Injectable, ExecutionContext } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { Reflector } from '@nestjs/core';
+import { IS_PUBLIC_KEY } from '../decorators/public.decorator';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  constructor(private reflector: Reflector) {
+    super();
+  }
+
+  canActivate(context: ExecutionContext) {
+    // Vérifier si la route est publique
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (isPublic) {
+      return true; // accès autorisé sans JWT
+    }
+
+    // Sinon, appliquer JwtAuthGuard normal
+    return super.canActivate(context);
+  }
+}
+
 // *****utilise*****
 // =============================================================================================================================
 // Sur toutes les routes nécessitant une authentification. Base de tous les autres guards.
